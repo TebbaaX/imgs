2d2cc609378bd4a70161878a6aff360953992104
adnane-tebbaa <58398485+adnane-X-tebbaa@users.noreply.github.com>
2020-11-08 21:35:43 +0100

    Not A big Deal

---
+++ sh.sh
@@ -0,0 +1,6 @@
+echo 
+echo Downloading https://www.github.com/adnane-x-tebbaa/imgs...
+echo git clone https://www.github.com/adnane-x-tebbaa/imgs
+git clone https://www.github.com/adnane-x-tebbaa/imgs 
+echo 
+echo Done...Enjoy!
